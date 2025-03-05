b8b290360e0564627aa0f70357e8c8cdaf65ed67
sabinbabu <sabinacharya444@gmail.com>
2025-03-05 14:53:19 +1100

    create route added for transaction

--- src/model/transactionModel.js
+++ src/model/transactionModel.js
@@ -1,5 +1,9 @@
 import transactionModel from "../schema/transactionSchema";
 
+export const createTransaction = (transactionObject) => {
+  return transactionModel(transactionObject).save();
+};
+
 export const getTransactions = (userId) => {
   return transactionModel.find({ userId });
 };
--- src/router/transactionRouter.js
+++ src/router/transactionRouter.js
@@ -4,13 +4,36 @@ import {
   buildSuccessResponse,
 } from "../utility/responseHelper";
 import { findUserById } from "../model/userModel";
-import { getTransactions } from "../model/transactionModel";
+import { createTransaction, getTransactions } from "../model/transactionModel";
 const transactionRouter = express.Router();
+
+// POST | CREATE Transactions
+transactionRouter.post("/", async (req, res) => {
+  try {
+    const { authorization } = req.headers;
+    const { title, type, amount, date } = req.body;
+    const user = await findUserById(authorization);
+    if (!user._id) {
+      return buildErrorResponse(res, "User not authorized");
+    }
+    const transaction = await createTransaction({ title, type, amount, date });
+    transaction._id
+      ? buildSuccessResponse(
+          res,
+          transaction,
+          "Transaction created successfully"
+        )
+      : buildErrorResponse(res, "Could not create transaction");
+  } catch (error) {
+    buildErrorResponse(res, "Something went wrong");
+    console.log(error);
+  }
+});
 
 // INDEX |  GET Transactions
 transactionRouter.get("/", async (req, res) => {
   try {
-    const { authorization } = req.header;
+    const { authorization } = req.headers;
     const user = await findUserById(authorization);
     if (!user._id) {
       return buildErrorResponse(res, "User not authorized");
